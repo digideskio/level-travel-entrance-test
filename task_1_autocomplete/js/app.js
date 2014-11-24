290dc03298516cf69fda15321604432b5e27fd06
Vitaly Pavlenko <cxielamiko@gmail.com>
2014-11-24 20:43:07 +0300

    Task 1 is working

--- task_1_autocomplete/js/app.js
+++ task_1_autocomplete/js/app.js
@@ -1,17 +1,22 @@
 var app = angular.module('travelAutocomplete', ['ngResource']);
 
 app.factory("Hotels", function($resource) {
-    return $resource("https://level.travel/papi/references/hotels?"
-                     "js=true&key=da4ad28030cfa998eedb7da1943e1b37&"
-                     "api_version=2&query=:query");
+    return $resource(
+        "https://level.travel/papi/references/hotels?js=true" +
+        "&key=da4ad28030cfa998eedb7da1943e1b37&api_version=2&query=:query",
+        {},
+        {
+            search: {method: 'GET', isArray: true}
+        }
+    );
 });
 
 app.controller("HotelsAutocomplete", function($scope, Hotels) {
     $scope.hotels = [];
 
     $scope.refresh = function() {
-        Hotels.get({
-            query: query
+        Hotels.search({
+            query: $scope.query
         }, function(data) {
             $scope.hotels = data;
         });
